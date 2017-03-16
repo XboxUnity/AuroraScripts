fdd0000ae30e5cc509a45484756d972501a8b72f
Gregory Herendi <GregHerendi@gmail.com>
2017-03-16 15:35:19 +0100

    Typo fix: Compare by Name (as a fallback) if  _ANY_  of the compared Item's ReleaseDate is unknown. If one is known, it cannot be compared to the missing other.
    Signed-off-by: Gregory Herendi <GregHerendi@gmail.com>

--- Sorts/ReleaseDate.lua
+++ Sorts/ReleaseDate.lua
@@ -1,11 +1,11 @@
 GameListSorters["Release Date"] = function(Item1, Item2, Type)
 	if Type == SortType.Descending then
-		if (Item1.ReleaseDate == "" and Item2.ReleaseDate == "") or (Item1.ReleaseDate == Item2.ReleaseDate) then
+		if (Item1.ReleaseDate == "" or Item2.ReleaseDate == "") or (Item1.ReleaseDate == Item2.ReleaseDate) then
 			return string.lower(Item1.Name) > string.lower(Item2.Name)
 		end
 		return Item1.ReleaseDate > Item2.ReleaseDate
 	end
-	if (Item1.ReleaseDate == "" and Item2.ReleaseDate == "") or (Item1.ReleaseDate == Item2.ReleaseDate) then
+	if (Item1.ReleaseDate == "" or Item2.ReleaseDate == "") or (Item1.ReleaseDate == Item2.ReleaseDate) then
 		return string.lower(Item1.Name) < string.lower(Item2.Name)
 	end
 	return Item1.ReleaseDate < Item2.ReleaseDate
