fbdfb43ab760f70e7eb666f0a172d5cbab8d22c2
DerfJagged <24844708+DerfJagged@users.noreply.github.com>
2024-07-29 15:35:35 -0500

    Revert version number

--- UtilityScripts/XefuSpoofer/Main.lua
+++ UtilityScripts/XefuSpoofer/Main.lua
@@ -1,6 +1,6 @@
 scriptTitle = "Xefu Spoofer"
 scriptAuthor = "Derf"
-scriptVersion = 2
+scriptVersion = 1
 scriptDescription = "Forces original Xbox games to use your selected xefu version. Compatibility list: ConsoleMods.org/fusion"
 scriptIcon = "icon.png"
 scriptPermissions = { "filesystem" }
